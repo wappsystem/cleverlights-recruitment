07b4375e92ede1a212ea3dd68bab647f189fb70e
Zhiming Zhou <zzm.vmiis@gmail.com>
2019-08-20 14:38:16 +1000

    update

--- components/cleverlights.js
+++ components/cleverlights.js
@@ -1,6 +1,16 @@
 (function(){
     var modules={
-        "panel-main-cleverlights":    			 {url:"$A/modules/elig-questions-data.html",Table:"cleverlight-recruitment-record"},
-   }
-    for(m in modules){$vm.module_list[m]=modules[m];}
+        "panel-main-cleverlights":    			 {url:"$H/modules/elig-questions-data.html",Table:"cleverlights-recruitment-record"},
+    }
+    //for(m in modules){$vm.module_list[m]=modules[m];}
+
+    for(p in modules){
+        $vm.module_list[p]=modules[p];
+        $vm.module_list[p].url=$vm.module_list[p].url.replace('$H',$vm.hosting_path);
+    }
+    if(window.location.toString().indexOf('tb=demo')!=-1){
+        for(p in $vm.module_list){
+            $vm.module_list[p].Table="demo-"+$vm.module_list[p].Table;
+        }
+    }
 })();
