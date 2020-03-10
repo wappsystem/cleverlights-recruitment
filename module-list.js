90264e738bb7c2ffa5455ae1aa1ffcc3a15321ba
Gunnar Unger <gunnar.unger@gmail.com>
2020-03-10 16:44:48 +1100

    update

--- module-list.js
+++ module-list.js
@@ -2,7 +2,8 @@
     var modules={
         "panel-main-cleverlights":    			 {url:"$H/m/elig-questions-data.html",Table:"cleverlights-recruitment-record",form_module:"elig-questions-form"},
         "elig-questions-form":    			 {url:"$H/m/elig-questions-form.html",Table:"cleverlights-recruitment-record"},
-        "panel-main-cleverlights-eligible":      {url:"$H/m/elig-questions-eligible-data.html",Table:"cleverlights-recruitment-record"},
+        "panel-main-cleverlights-eligible":      {url:"$H/m/elig-questions-eligible-data.html",Table:"cleverlights-recruitment-record",form_module:"panel-main-cleverlights-eligible-form"},
+        "panel-main-cleverlights-eligible-form":		    {url:"$H/m/elig-questions-eligible-form.html",Table:"cleverlights-recruitment-record"},
         "panel-main-cleverlights-non-eligible":  {url:"$H/m/elig-questions-non-eligible-data.html",Table:"cleverlights-recruitment-record"},
     }
     //for(m in modules){$vm.module_list[m]=modules[m];}
